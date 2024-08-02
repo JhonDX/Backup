db4705cb1f4a81935228f92a0783bc7936239bfc
JhonDX <149755482+JhonDX@users.noreply.github.com>
2024-08-02 16:37:42 -0300

    Atualização
    Inclui a pasta log, a copia dos backups estão funcionando, porém a copia de backups que contem data é bem complicado...precisa deixar bem claro na config.ini
    precisa fracionar o nome do backup ex: \\SERVIDOR\%NOME_BACKUP%%DATA%%EXTENSÃO%, A data é pega pelo dia no sistema ele faz essa inclusão para ser o máximo automático possível. em breve faço um video explicando.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from datetime import date
 import configparser
 import shutil
+from tkinter import *
 
 #COPIAR BACKUP
 #ENVIAR PARA NUVEM
@@ -10,19 +11,17 @@ import shutil
 
 #USER UPLOAD SUPORTE@GACCBAHIA.ORG.BR
 
-
-#TIME
+# TIME
 d = date.today().day
 m = date.today().month
 ano = date.today().year
 
-if d and m <=9:
-    dia =str(f'{d:02}')
+if d and m <= 9:
+    dia = str(f'{d:02}')
     mes = str(f'{m:02}')
 else:
     pass
-YMD =str(f'{dia}-{mes}-{ano}')
-
+YMD = str(f'{dia}-{mes}-{ano}')
 
 # Criação do objeto ConfigParser
 configad = configparser.ConfigParser()
@@ -31,28 +30,36 @@ configad = configparser.ConfigParser()
 configad.read('config.ini')
 BACKUP = configad['DIR_BACKUP']['BACKUP']
 
-
-#CRIANDO PASTA DO BACKUP
+# CRIANDO PASTA DO BACKUP
 if os.path.exists(f'{BACKUP}') == False:
     os.mkdir(f'{BACKUP}')
 else:
     pass
 
-#CRIANDO PASTA DO ANO
+# CRIANDO PASTA DO ANO
 if os.path.exists(f'{BACKUP}/{ano}') == False:
     os.mkdir(f'{BACKUP}/{ano}')
 else:
     pass
 
-#CRIANDO PASTA DO MÊS + ANO
+# CRIANDO PASTA DO MÊS + ANO
 if os.path.exists(f'{BACKUP}/{ano}/{mes}-{ano}') == False:
     os.mkdir(f'{BACKUP}/{ano}/{mes}-{ano}')
 else:
     pass
 
-#CRIANDO PASTA DO DIA + MÊS + ANO
+# CRIANDO PASTA DO DIA + MÊS + ANO
 if os.path.exists(f'{BACKUP}/{ano}/{mes}-{ano}/{dia}-{mes}-{ano}') == False:
     os.mkdir(f'{BACKUP}/{ano}/{mes}-{ano}/{dia}-{mes}-{ano}')
+else:
+    pass
+if os.path.exists(f'{BACKUP}/{ano}/{mes}-{ano}/{dia}-{mes}-{ano}') == False:
+    os.mkdir(f'{BACKUP}/{ano}/{mes}-{ano}/{dia}-{mes}-{ano}')
+    os.mkdir(f'{BACKUP}/{ano}/{mes}-{ano}/{dia}-{mes}-{ano}/log')
+else:
+    pass
+if os.path.exists(f'{BACKUP}/{ano}/{mes}-{ano}/{dia}-{mes}-{ano}/log') == False:
+    os.mkdir(f'{BACKUP}/{ano}/{mes}-{ano}/{dia}-{mes}-{ano}/log')
 else:
     pass
 
@@ -60,9 +67,11 @@ else:
 DIR_BACKUP_SAVE = f'{BACKUP}/{ano}/{mes}-{ano}/{dia}-{mes}-{ano}/'
 
 
-#COPIA COM DATA
+
+
+# COPIA COM DATA
 COUNT = 1
-MAX_COUNT = 50 # Defina o valor máximo para COUNT conforme necessário
+MAX_COUNT = 50  # Defina o valor máximo para COUNT conforme necessário
 
 while COUNT <= MAX_COUNT:
     try:
@@ -71,7 +80,7 @@ while COUNT <= MAX_COUNT:
         EXT = configad['EXT_ARQUIVO_COM_DATA'][f'EXT_{COUNT}']
         SRC = str(f'{DIR_BACKUP}{DIR_NOME}{YMD}{EXT}')
         DST = str(f'{DIR_BACKUP_SAVE}{DIR_NOME}{YMD}{EXT}')
-        arquivo = shutil.copyfile(src=SRC,dst=DST)
+        arquivo = shutil.copyfile(src=SRC, dst=DST)
         print(f'Copiando Arquivo!: {arquivo}')
 
     except KeyError as erro:
@@ -83,18 +92,19 @@ while COUNT <= MAX_COUNT:
     COUNT += 1
 
 COUNT = 1
-MAX_COUNT = 50 # Defina o valor máximo para COUNT conforme necessário
+MAX_COUNT = 50  # Defina o valor máximo para COUNT conforme necessário
 
-
-
-#COPIA DOS BACKUP SEM DATA
+# COPIA DOS BACKUP SEM DATA
 while COUNT <= MAX_COUNT:
     try:
         DIR_BACKUP = configad['NOME_BACKUP'][f'DIR_BACKUP_{COUNT}']
         DIR_NOME = configad['NOME_ARQUIVOS'][f'DIR_BACKUP_{COUNT}']
         # Faça algo com DIR_LOCAL e DIR_BACKUP
-        arquivo = shutil.copyfile(src=DIR_BACKUP,dst=f'{DIR_BACKUP_SAVE}/{DIR_NOME}')
-        print(f'Copiando Arquivo!: {arquivo}')
+        try:
+            arquivo = shutil.copyfile(src=DIR_BACKUP, dst=f'{DIR_BACKUP_SAVE}/{DIR_NOME}')
+            print(f'Copiando Arquivo!: {arquivo}')
+        except:
+            pass
 
 
     except KeyError as erro:
@@ -103,9 +113,3 @@ while COUNT <= MAX_COUNT:
         break  # Sai do loop se uma chave não for encontrada
 
     COUNT += 1
-
-
-
-
-
-
